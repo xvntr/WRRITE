98644ee37cf49ed3726c463b8c42850b693ab073
xvntr <164462454+xvntr@users.noreply.github.com>
2024-03-22 22:05:47 +0700

    store artikel ke db

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Story extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'author', 'content', 'image'];
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,23 @@
 <?php
 
+use App\Http\Requests\WriteValidation;
 use App\Models\User;
 use App\Models\Story;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage', ['story' => Story::paginate(9)]);
+    return view('homepage', ['story' => Story::latest()->paginate(9)]);
 })->name('write.index');
 
+Route::get('/publish', function () {
+    return view('publish');
+})->name('write.publish');
+
 Route::get('/{id}', function ($id) {
-    return view('single-story', ['story' => Story::findOrFail($id)]);
+    return view('single', ['story' => Story::findOrFail($id)]);
 })->name('write.show');
+
+Route::post('/publish', function (WriteValidation $request) {
+    $story = Story::create($request->validated());
+    return redirect()->route('write.show', ['id' => $story->id]);
+})->name('write.store');
