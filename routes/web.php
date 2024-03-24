2f5f1d2dc59c079711880eff9b04053a4bdeda1d
xvntr <164462454+xvntr@users.noreply.github.com>
2024-03-24 12:50:34 +0700

    update controller semua

--- routes/web.php
+++ routes/web.php
@@ -8,34 +8,12 @@ use App\Http\Controllers\IndexController;
 
 
 Route::get('/', [IndexController::class, 'index'])->name('write.index');
+Route::get('/publish', [IndexController::class, 'publish'])->name('write.publish');
+Route::get('/{id}', [IndexController::class, 'show'])->name('write.show');
+Route::get('/{id}/edit', [IndexController::class, 'edit'])->name('write.edit');
 
-Route::get('/publish', function () {
-    return view('publish');
-})->name('write.publish');
-
-Route::get('/{id}', function ($id) {
-    return view('single', ['story' => Story::findOrFail($id)]);
-})->name('write.show');
-
-Route::get('/{id}/edit', function ($id) {
-    return view('edit', ['story' => Story::findOrFail($id)]);
-})->name('write.edit');
-
-Route::POST('/publish', function (WriteValidation $request) {
-    $story = Story::create($request->validated());
-    return redirect()->route('write.show', ['id' => $story->id]);
-})->name('write.store');
-
-Route::PUT('/{id}', function ($id, WriteValidation $request) {
-    $story = Story::findOrFail($id);
-    $story->update($request->validated());
-    return redirect()->route('write.show', ['id' => $story->id]);
-})->name('write.update');
-
-Route::DELETE('/{id}', function ($id, Story $story) {
-    $story = Story::findOrFail($id);
-    $story->delete();
-    return redirect()->route('write.index');
-})->name('write.destroy');
+Route::post('/publish', [IndexController::class, 'store'])->name('write.store');
+Route::put('/{id}', [IndexController::class, 'update'])->name('write.update');
+Route::delete('/{id}', [IndexController::class, 'destroy'])->name('write.destroy');
 
 
